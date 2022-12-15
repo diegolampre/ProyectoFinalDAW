7046ff344c6ed7ea876156db3158d44510109e42
diegolampre <lampreolivera@gmail.com>
2022-12-15 13:02:18 +0100

    Septimo commit | Agregando secciones de admin y enlaces

--- administrador/template/cabecera.php
+++ administrador/template/cabecera.php
@@ -10,13 +10,18 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
+
+    <?php $url="http://".$_SERVER['HTTP_HOST']."/sitioweb" ?>
+
         <nav class="navbar navbar-expand navbar-light bg-light">
             <div class="nav navbar-nav">
                 <a class="nav-item nav-link active" href="#">Administrador de PropaGames <span class="sr-only">(current)</span></a>
-                <a class="nav-item nav-link" href="#">Inicio</a>
-                <a class="nav-item nav-link" href="#">Productos</a>
-                <a class="nav-item nav-link" href="#">Usuarios</a>
-                <a class="nav-item nav-link" href="#">Ver sitio web</a>
+                <a class="nav-item nav-link" href="<?php echo $url ?>/administrador/inicio.php">Inicio</a>
+                <a class="nav-item nav-link" href="<?php echo $url ?>/administrador/seccion/productos.php">Productos</a>
+                <a class="nav-item nav-link" href="<?php echo $url ?>/administrador/seccion/usuarios.php">Usuarios</a>
+                <a class="nav-item nav-link" href="<?php echo $url ?>/administrador/seccion/cerrar.php">Cerrar</a>
+
+                <a class="nav-item nav-link" href="<?php echo $url; ?>">Ver sitio web</a>
             </div>
         </nav>
 
