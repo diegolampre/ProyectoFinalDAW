4d2ba70fc663582789dee5a04375e2d6e646399b
diegolampre <lampreolivera@gmail.com>
2022-12-15 01:01:27 +0100

    Primer commit | Esqueleto proyecto

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php   
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PropaGames</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+</head>
+<body>
+    <h3>PropaGames</h3>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nav 2</a>
+            </li>
+        </ul>
+    </nav>
+</body>
+</html>
