1681273a6b19c6bfdd311ef63bba8e0a7767928b
diegolampre <lampreolivera@gmail.com>
2022-12-15 11:17:08 +0100

    Segundo commit | añado cabecera y pie

---
+++ template/cabecera.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PropaGames</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
+</head>
+<body>
+    <h3>PropaGames</h3>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+
+            <li class="nav-item ">
+                <a class="nav-link" href="#">PropaGames</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Tienda</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Nosotros</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
